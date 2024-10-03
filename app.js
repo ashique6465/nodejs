6d5c029b092d456fb601e85678690692e167301b
ashique6465 <mdashiqueali71@gmail.com>
2024-10-03 21:51:43 +0530

    Server creation

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require('http');
+const express = require('express')
 
-
-
-const server = http.createServer((req,res)=>{
-    console.log(req);
-    process.exit();
+const app = express();
+app.use('/add-product',(req,res,next)=>{
+    console.log("In another middleware!");
+    res.send('<h1>Add Product</h1>');
 });
 
-server.listen(3000);
+app.use('/',(req,res,next)=>{
+    console.log("In another middleware!");
+    res.send('<h1>Hello from express</h1>');
+});
+app.listen(3000,(req,res)=>{
+    console.log("Server is running on port 3000")
+})
